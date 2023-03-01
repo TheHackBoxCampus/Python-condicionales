16343bf5fe1b45f0c80e42bfd0ab7dca10a41f66
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-01 17:33:01 -0500

    Ejercicio 9

--- Exercises.py
+++ Exercises.py
@@ -1,14 +1,23 @@
-#Ejercicio 8
-#Escriba un bloque cualquiera de código en Python en donde utilice 2 condicionales (if) anidados.
+#Ejercicio 9
+# Construya un algoritmo en Python, que permita ingresar la
+# información de un empleado e imprima el nombre, los apellidos y la antigüedad. 
+# Los datos que se deben solicitar
+# son los siguientes:
+#* Nombre 
+#* Teléfono 
+#* Año de ingreso a la empresa
+#* Apellidos 
+#* Edad.
 
-nombre = input("Ingrese su nombre: ")
-edad = int(input("Escribe tu edad: "))
+nombre = input("Digite su nombre ")
+apellido = input("Digite su apellido ")
+edad = input("Digite su edad ")
+telefono = int(input("Digite su numero de teléfono "))
+ingreso = int(input("Digite el año en el que ingreso a la empresa "))
+actual = 2023
+antiguedad = actual - ingreso
 
-if len(nombre) <= 2:
-    print("Ingrese un nombre que corresponda")    
+if nombre != "" and edad != "":
+    print(f"El empleado de nombre: {nombre} {apellido} de {edad} años de edad, con numero de telefono {telefono}, tiene una antiguedad de {antiguedad} años en la empresa")
 else:
-    if edad > 15:
-       print("Hola {} Puedes entrar a la montaña rusa".format(nombre))
-   
-    else: 
-        print("Hola {} No puedes entrar a la montaña rusa.".format(nombre))
+    print("Sus datos estan incorrectos")
