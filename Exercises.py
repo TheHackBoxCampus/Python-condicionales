08dbc74d74d6ef0a578ca75f2fff31fcda9effac
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-01 17:25:26 -0500

    Ejercicio 2

--- Exercises.py
+++ Exercises.py
@@ -1,16 +1,18 @@
-#Ejercicio 1 
-#1. Qué operadores utiliza Python en los siguientes casos:
+#Ejercicio 2 
+#2. En la jerarquía de operadores, cuáles son los que más
+# prioridad tienen cuando el intérprete de Python los evalúa?
 
-#A. División Modular                %
-#B. Exponenciación                  **
-#C. División que retorne entero.    //
+def prioridad():
+    return print(
+    f""" 
+    Mayor prioridad
+    ---------------
+    1 Operaciones entre parentesis \t\t\t\t\t\t () 
+    2 Potencia \t\t\t\t\t\t\t\t\t ** 
+    3 Multiplicación y División, módulo o residuo, División entenera       *, /, %, // 
+    4 Suma y resta \t\t\t\t\t\t\t\t+, -
+    --------------
+    Menor prioridad
+    """)
 
-def operadores():
-    numero1 = int(input(f"Digite el primer numero: "))
-    numero2 = int(input(f"Digite el segundo numero: "))
-
-    print(f"\nLa división modular de {numero1} % {numero2} = {numero1 % numero2}") 
-    print(f"La exponenciación de {numero1} elevado a {numero2} = {numero1 ** numero2}") 
-    print(f"La división de enteros es: {numero1} // {numero2} = {numero1 // numero2}") 
-
-operadores()
+prioridad()
