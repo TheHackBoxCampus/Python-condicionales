b4a8c05e3263f0c79aff86f9806fcb9aa9f88144
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-01 17:30:39 -0500

    Ejercicio 6

--- Exercises.py
+++ Exercises.py
@@ -1,26 +1,16 @@
-#Ejercicio 5
-#Enumere 5 tipos de datos en Python y suministre un valor de ejemplo de cada uno.
+#Ejercicio 6
+#En sus propias palabras, qué son las funciones preconstruidas y proporcione 2 ejemplos.
 
-def tiposDeDatos():
-    str = "Miller Nariño"
-    int = 10
-    float = 9.8
-    bool = False
-    list = [1, str, int, float, bool]
-    dicts = {"nombre": "miller"}
-    tuples = (1,2,3,4)
+lista = [5,10,23]
+Exmaple2 = [1.2]
+numeroMaximo = max(lista)
+redondeo = round(Exmaple2[0])
 
-    print(
+print(
     f"""
-    -----------------------------------------------
-    tipo Entero: {int}
-    tipo Float: {float}
-    tipo Booleano: {bool}
-    tipo Cadena: {str}
-    tipoLista: {list}
-    tipo tupla: {tuples}
-    tipo diccionario: {dicts}
-    -----------------------------------------------
+    --------------------------------------------------
+    Son funciones que ya estan incorporadas en python: 
+    La funcion ROUND(): {redondeo}
+    la funcion MAX(): {numeroMaximo}
+    --------------------------------------------------
     """)
-
-tiposDeDatos()
