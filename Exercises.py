5cce6d949aab10e568a0e35c57e9ede499c28ce7
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-03-01 17:28:49 -0500

    Ejercicio 5

--- Exercises.py
+++ Exercises.py
@@ -1,11 +1,26 @@
-#Ejercicio 4
-#Que son las expresiones regulares
-import re
+#Ejercicio 5
+#Enumere 5 tipos de datos en Python y suministre un valor de ejemplo de cada uno.
 
-Ejemplo = re.compile('a[3-5]+') # coincide con una letra, seguida de al menos 1 dígito entre 3 y 5
+def tiposDeDatos():
+    str = "Miller Nariño"
+    int = 10
+    float = 9.8
+    bool = False
+    list = [1, str, int, float, bool]
+    dicts = {"nombre": "miller"}
+    tuples = (1,2,3,4)
 
-print("""
-------------------------------------------------------------
-Una expresión regular es una cadena de caracteres que es utilizada para describir o encontrar patrones dentro de otros strings
-------------------------------------------------------------
-""")
+    print(
+    f"""
+    -----------------------------------------------
+    tipo Entero: {int}
+    tipo Float: {float}
+    tipo Booleano: {bool}
+    tipo Cadena: {str}
+    tipoLista: {list}
+    tipo tupla: {tuples}
+    tipo diccionario: {dicts}
+    -----------------------------------------------
+    """)
+
+tiposDeDatos()
